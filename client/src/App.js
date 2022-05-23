6682fbd4151db35d1183deb410fabbeeed7851ab
kittymercury <eissturm.rrr@gmail.com>
2022-05-23 15:34:14 +0300

    Fix: client started from list page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 export default class App extends React.Component {
+  componentDidMount = () => {
+    const location = window.location
+
+    if (location.pathname === '/') {
+      window.location.assign(`${location.href}articles`)
+    }
+  }
+  
   render() {
     return (
       <div className="test-task">
